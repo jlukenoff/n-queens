001e3938a649806d26733f4332ecc34c9c994a62
jlukenoff <jlukenoff@gmail.com>
2018-06-20 17:11:45 -0700

    attempts findNRooks

--- src/solvers.js
+++ src/solvers.js
@@ -16,9 +16,47 @@
 
 
 window.findNRooksSolution = function(n) {
-  var solution = undefined; //fixme
+  //make empty board(n)
+  let emptyBoard = new Board({n: n});
+  let counter = 0;
+  
+  
+  
+  //create a helper function: takes a populated board and recursive counter
+  const recursiveRooks = function(board, callCount, prev, erase) {
+    //iterate through row
+    for (let row = 0; row < n; row++) {
+      //iterate through columns
+      for (let col = 0; col < n; col++) {
+        // check if position is populated, proceed with loop
+        if (board.get(row)[col] !== 0) { continue; }
+        
+        //store prevPosition row, col
+        if (erase) {
+          board.togglePiece(prev[0], prev[1]);
+        }
+        
+        //toggle position
+        board.togglePiece(row, col);
+        //check if counter is equal to n to stop recursion
+        if (callCount < n) {
+          //recursively call helper func with board argument and counter++
+          recursiveRooks(board, callCount++, [row, col], erase);
+        // check if valid board and return board
+        } 
+        if (!board.hasAnyRooksConflicts()) {
+          return board.rows();
+        }
+        erase = true;
+      }
+    }
+    //decrement counter
+    callCount--;
+  };
+  var solution = recursiveRooks(emptyBoard, 1, _, false);
 
-  console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
+  // console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
+  console.log(solution);
   return solution;
 };
 
